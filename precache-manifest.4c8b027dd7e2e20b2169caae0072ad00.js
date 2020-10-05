a14c415e832fb7f15b9d2b0d4d50d64193def533
llxlr <yanghongday369@gmail.com>
2020-10-05 11:24:38 +0000

    Deploying to gh-pages from master 1fbc3435f6a9af3f4a26054eb60ccd1eec399c92

---
+++ precache-manifest.4c8b027dd7e2e20b2169caae0072ad00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fbfbd29c0c045d71742f97f1a2edd10",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "6a928ef62a78f8b908fb",
+    "url": "/github-actions-demo/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "6d0846b977357c549999",
+    "url": "/github-actions-demo/static/js/2.6e372feb.chunk.js"
+  },
+  {
+    "revision": "6a928ef62a78f8b908fb",
+    "url": "/github-actions-demo/static/js/main.1690ce7a.chunk.js"
+  },
+  {
+    "revision": "41870333dfc813cf444c",
+    "url": "/github-actions-demo/static/js/runtime~main.dd1e6690.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/github-actions-demo/static/media/logo.06e73328.svg"
+  }
+]);
